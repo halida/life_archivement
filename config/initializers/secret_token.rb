c64178ace33b44560c9bf3f3c608a61689030d99
linjunhalida <linjunhalida@gmail.com>
2011-08-15 13:43:44 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LifeArchivement::Application.config.secret_token = 'aaf7cd5988364d8a0c9430fcd904a5c3bbc5e6ca5e1d7675c95c7752d358b22f9f25b2dc1397d0fae21fe783c7320cbd8e5f1ce8dfc3b512a74a894ba98bd495'
